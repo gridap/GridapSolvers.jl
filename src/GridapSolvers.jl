154077743ac2e6418e58b75d5200725880f6f1ae
JordiManyer <j.manyer@hotmail.com>
2022-10-20 10:52:21 +1100

    Added exports

--- src/GridapSolvers.jl
+++ src/GridapSolvers.jl
@@ -4,15 +4,28 @@ module GridapSolvers
   using LinearAlgebra
   using Gridap
   using Gridap.Helpers
+  using Gridap.Geometry
+  using Gridap.FESpaces
   using PartitionedArrays
   using GridapDistributed
   using GridapP4est
 
 
-
   export ModelHierarchy
+  export num_levels, get_level, get_model, get_model_before_redist, has_refinement, has_redistribution
+
+  export FESpaceHierarchy
+  export get_space, get_space_before_redist
+
+  export InterGridTransferOperator
+  export RestrictionOperator, ProlongationOperator
+  export setup_transfer_operators
 
   include("PartitionedArraysExtensions.jl")
   include("ModelHierarchies.jl")
+  include("FESpaceHierarchies.jl")
+  include("RedistributeTools.jl")
+  include("InterGridTransferOperators.jl")
+
 
 end
