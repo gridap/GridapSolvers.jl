0e626022447647d476d7d3f2f44c7b6e09757136
JordiManyer <jordi.manyer@monash.edu>
2023-10-03 23:12:16 +1100

    Minor

--- src/SolverInterfaces/GridapExtras.jl
+++ src/SolverInterfaces/GridapExtras.jl
@@ -1,6 +1,6 @@
 
 # LinearSolvers that depend on the non-linear solution
-
+"""
 function Gridap.Algebra.numerical_setup!(ns::Gridap.Algebra.LinearSolver,A::AbstractMatrix,x::AbstractVector)
   numerical_setup!(ns,A)
 end
@@ -8,3 +8,4 @@ end
 function allocate_solver_caches(ns::Gridap.Algebra.LinearSolver,args...;kwargs...)
   @abstractmethod
 end
+"""
