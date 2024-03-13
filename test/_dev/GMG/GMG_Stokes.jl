7465dba2ee56de0a8c54d13abb20c2c4bb7e90d2
JordiManyer <jordi.manyer@monash.edu>
2024-03-13 13:56:26 +1100

    Added more test scripts

---
+++ test/_dev/GMG/GMG_Stokes.jl
@@ -0,0 +1,175 @@
+using Test
+using LinearAlgebra
+using FillArrays, BlockArrays
+
+using Gridap
+using Gridap.ReferenceFEs, Gridap.Algebra, Gridap.Geometry, Gridap.FESpaces
+using Gridap.CellData, Gridap.MultiField
+using PartitionedArrays
+using GridapDistributed
+using GridapP4est
+
+using GridapSolvers
+using GridapSolvers.LinearSolvers
+using GridapSolvers.MultilevelTools
+using GridapSolvers.PatchBasedSmoothers
+
+function get_hierarchy_matrices(trials,tests,biform,qdegree)
+  nlevs = num_levels(trials)
+  mh    = trials.mh
+
+  mats = Vector{PSparseMatrix}(undef,nlevs)
+  for lev in 1:nlevs
+    parts = get_level_parts(mh,lev)
+    if i_am_in(parts)
+      model = get_model(mh,lev)
+      U = GridapSolvers.MultilevelTools.get_fe_space(trials,lev)
+      V = GridapSolvers.MultilevelTools.get_fe_space(tests,lev)
+      Ω = Triangulation(model)
+      dΩ = Measure(Ω,qdegree)
+      ai(u,v) = biform(u,v,dΩ)
+      mats[lev] = assemble_matrix(ai,U,V)
+    end
+  end
+  return mats
+end
+
+function get_patch_smoothers(tests,patch_decompositions,biform,qdegree)
+  mh = tests.mh
+  patch_spaces = PatchFESpace(tests,patch_decompositions)
+  nlevs = num_levels(mh)
+  smoothers = Vector{RichardsonSmoother}(undef,nlevs-1)
+  for lev in 1:nlevs-1
+    parts = get_level_parts(mh,lev)
+    if i_am_in(parts)
+      PD = patch_decompositions[lev]
+      Ph = GridapSolvers.MultilevelTools.get_fe_space(patch_spaces,lev)
+      Vh = GridapSolvers.MultilevelTools.get_fe_space(tests,lev)
+      Ω  = Triangulation(PD)
+      dΩ = Measure(Ω,qdegree)
+      ap(u,v) = biform(u,v,dΩ)
+      patch_smoother = PatchBasedLinearSolver(ap,Ph,Vh)
+      smoothers[lev] = RichardsonSmoother(patch_smoother,10,0.2)
+    end
+  end
+  return smoothers
+end
+
+function get_mesh_hierarchy(parts,nc,np_per_level)
+  Dc = length(nc)
+  domain = (Dc == 2) ? (0,1,0,1) : (0,1,0,1,0,1)
+  num_refs_coarse = (Dc == 2) ? 1 : 0
+  
+  num_levels   = length(np_per_level)
+  cparts       = generate_subparts(parts,np_per_level[num_levels])
+  cmodel       = CartesianDiscreteModel(domain,nc)
+
+  labels = get_face_labeling(cmodel)
+  add_tag_from_tags!(labels,"top",[3,4,6])
+  add_tag_from_tags!(labels,"walls",[1,2,5,7,8])
+
+  coarse_model = OctreeDistributedDiscreteModel(cparts,cmodel,num_refs_coarse)
+  mh = ModelHierarchy(parts,coarse_model,np_per_level)
+  return mh
+end
+
+np = 1
+nc = (8,8)
+np_per_level = [np,np]
+parts = with_mpi() do distribute
+  distribute(LinearIndices((np,)))
+end
+mh = get_mesh_hierarchy(parts,nc,np_per_level);
+model = get_model(mh,1)
+
+order = 2
+qdegree = 2*(order+1)
+Dc = length(nc)
+
+reffe_u = ReferenceFE(lagrangian,VectorValue{2,Float64},order)
+reffe_p = ReferenceFE(lagrangian,Float64,order-1;space=:P)
+
+tests_u  = TestFESpace(mh,reffe_u,dirichlet_tags=["walls","top"]);
+trials_u = TrialFESpace(tests_u,[VectorValue(0.0,0.0),VectorValue(1.0,0.0)]);
+
+U = GridapSolvers.MultilevelTools.get_fe_space(trials_u,1)
+V = GridapSolvers.MultilevelTools.get_fe_space(tests_u,1)
+Q = TestFESpace(model,reffe_p;conformity=:L2,constraint=:zeromean) 
+
+mfs = Gridap.MultiField.BlockMultiFieldStyle()
+X = MultiFieldFESpace([U,Q];style=mfs)
+Y = MultiFieldFESpace([V,Q];style=mfs)
+
+α = 1.e6
+f = VectorValue(1.0,1.0)
+Π_Qh = LocalProjectionMap(QUAD,lagrangian,Float64,order-1;quad_order=qdegree,space=:P)
+graddiv(u,v,dΩ) = ∫(α*Π_Qh(divergence(u))⋅Π_Qh(divergence(v)))dΩ
+biform_u(u,v,dΩ) = ∫(∇(v)⊙∇(u))dΩ + graddiv(u,v,dΩ)
+biform((u,p),(v,q),dΩ) = biform_u(u,v,dΩ) - ∫(divergence(v)*p)dΩ - ∫(divergence(u)*q)dΩ
+liform((v,q),dΩ) = ∫(v⋅f)dΩ
+
+patch_decompositions = PatchDecomposition(mh)
+smoothers = get_patch_smoothers(tests_u,patch_decompositions,biform_u,qdegree)
+smatrices = get_hierarchy_matrices(trials_u,tests_u,biform_u,qdegree);
+
+Ω = Triangulation(model)
+dΩ = Measure(Ω,qdegree)
+a(u,v) = biform(u,v,dΩ)
+l(v) = liform(v,dΩ)
+op = AffineFEOperator(a,l,X,Y)
+A, b = get_matrix(op), get_vector(op);
+Auu = blocks(A)[1,1]
+
+restrictions = setup_restriction_operators(tests_u,qdegree;mode=:residual,solver=LUSolver());
+prolongations = setup_patch_prolongation_operators(tests_u,patch_decompositions,biform_u,graddiv,qdegree);
+
+gmg = GMGLinearSolver(mh,
+                      smatrices,
+                      prolongations,
+                      restrictions,
+                      pre_smoothers=smoothers,
+                      post_smoothers=smoothers,
+                      coarsest_solver=LUSolver(),
+                      maxiter=4,
+                      rtol=1.0e-8,
+                      verbose=true,
+                      mode=:preconditioner)
+gmg.log.depth += 1
+
+solver_u = FGMRESSolver(5,gmg;maxiter=20,atol=1e-14,rtol=1.e-6,verbose=i_am_main(parts))
+ns_u = numerical_setup(symbolic_setup(solver_u,Auu),Auu)
+
+x_u = pfill(0.0,partition(axes(Auu,2)))
+b_u = blocks(b)[1]
+solve!(x_u,ns_u,b_u)
+
+# Solve
+
+solver_p = CGSolver(RichardsonSmoother(JacobiLinearSolver(),10,0.2);maxiter=20,atol=1e-14,rtol=1.e-6,verbose=false)
+
+using GridapSolvers.BlockSolvers: LinearSystemBlock, BiformBlock, BlockTriangularSolver
+diag_blocks  = [LinearSystemBlock(),BiformBlock((p,q) -> ∫(-1.0/α*p*q)dΩ,Q,Q)]
+bblocks = map(CartesianIndices((2,2))) do I
+  (I[1] == I[2]) ? diag_blocks[I[1]] : LinearSystemBlock()
+end
+coeffs = [1.0 1.0;
+          0.0 1.0]  
+P = BlockTriangularSolver(bblocks,[solver_u,solver_p],coeffs,:upper)
+solver = FGMRESSolver(20,P;atol=1e-14,rtol=1.e-6,verbose=i_am_main(parts))
+ns = numerical_setup(symbolic_setup(solver,A),A)
+
+x = Gridap.Algebra.allocate_in_domain(A); fill!(x,0.0)
+solve!(x,ns,b)
+
+# Postprocess
+
+model = get_model(mh,1)
+Ω = Triangulation(model)
+dΩ = Measure(Ω,qdegree)
+
+U = get_fe_space(trials,1)
+uh = FEFunction(U,x)
+
+uh_exact = interpolate(u_exact,U)
+eh = uh - uh_exact
+E = sqrt(sum(∫(eh⋅eh)dΩ))
---
+++ test/_dev/GMG/GMG_prolongator.jl
@@ -0,0 +1,218 @@
+using Gridap
+using Gridap.Geometry, Gridap.FESpaces, Gridap.Adaptivity, Gridap.ReferenceFEs, Gridap.Arrays
+using Gridap.CellData, Gridap.Fields
+
+using GridapSolvers
+using GridapSolvers.LinearSolvers
+using GridapSolvers.MultilevelTools
+using GridapSolvers.PatchBasedSmoothers
+
+using LinearAlgebra
+
+order = 3
+poly  = QUAD
+
+# Geometry 
+n = 6
+cmodel = CartesianDiscreteModel((0,1,0,1),(n,n))
+if poly == TRI
+  cmodel = simplexify(cmodel)
+end
+fmodel = refine(cmodel)
+
+Ωh = Triangulation(fmodel)
+ΩH = Triangulation(cmodel)
+
+qdegree = 2*(order+1)
+dΩh = Measure(Ωh,qdegree)
+dΩH = Measure(ΩH,qdegree)
+dΩHh = Measure(ΩH,Ωh,qdegree)
+
+# Spaces
+conformity = H1Conformity()
+u_exact(x) = VectorValue(x[1]^2,-2.0*x[2]*x[1])
+#u_exact(x) = VectorValue(x[1]*(x[1]-1.0)*x[2]*(x[2]-1.0),(1.0-2.0*x[1])*(1.0/3.0*x[2]^3 - 1.0/2.0*x[2]^2))
+
+reffe = ReferenceFE(lagrangian,VectorValue{2,Float64},order)
+VH = TestFESpace(cmodel,reffe,dirichlet_tags="boundary")
+UH = TrialFESpace(VH,u_exact)
+Vh = TestFESpace(fmodel,reffe,dirichlet_tags="boundary")
+Uh = TrialFESpace(Vh,u_exact)
+
+# Weakform
+α = 1.e8
+f(x) = -Δ(u_exact)(x)
+Π_Qh = LocalProjectionMap(poly,lagrangian,Float64,order-1;quad_order=qdegree,space=:P)
+lap(u,v,dΩ) = ∫(∇(v)⊙∇(u))dΩ
+graddiv(u,v,dΩ) = ∫(α*Π_Qh(divergence(v))⋅Π_Qh(divergence(u)))dΩ
+biform(u,v,dΩ) = lap(u,v,dΩ) + graddiv(u,v,dΩ)
+ah(u,v) = biform(u,v,dΩh)
+aH(u,v) = biform(u,v,dΩH)
+lh(v) = ∫(v⋅f)*dΩh
+lH(v) = ∫(v⋅f)*dΩH
+
+oph = AffineFEOperator(ah,lh,Uh,Vh)
+opH = AffineFEOperator(aH,lH,UH,VH)
+
+xh_star = get_free_dof_values(solve(oph))
+xH_star = get_free_dof_values(solve(opH))
+
+Ah, bh = get_matrix(oph), get_vector(oph);
+AH, bH = get_matrix(opH), get_vector(opH);
+
+Mhh = assemble_matrix((u,v)->∫(u⋅v)*dΩh,Vh,Vh)
+
+function project_c2f(xH)
+  uH = FEFunction(VH,xH)
+  op = AffineFEOperator((u,v)->∫(u⋅v)*dΩh,v->∫(v⋅uH)*dΩh,Vh,Vh)
+  return get_matrix(op)\get_vector(op)
+end
+
+function project_f2c(rh)
+  Qrh = Mhh\rh
+  uh  = FEFunction(Vh,Qrh)
+  assemble_vector(v->∫(v⋅uh)*dΩHh,VH)
+end
+
+function interp_c2f(xH)
+  get_free_dof_values(interpolate(FEFunction(VH,xH),Vh))
+end
+
+# Smoother
+
+PD = PatchDecomposition(fmodel)
+Ph = PatchFESpace(Vh,PD,reffe;conformity)
+Ωp = Triangulation(PD)
+dΩp = Measure(Ωp,qdegree)
+ap(u,v) = biform(u,v,dΩp)
+smoother = RichardsonSmoother(PatchBasedLinearSolver(ap,Ph,Vh),20,0.2)
+smoother_ns = numerical_setup(symbolic_setup(smoother,Ah),Ah)
+
+
+# New prolongation operator
+ftopo = get_grid_topology(fmodel)
+n2e_map = Gridap.Geometry.get_faces(ftopo,0,1)
+e2n_map = Gridap.Geometry.get_faces(ftopo,1,0)
+ccoords = get_node_coordinates(cmodel)
+fcoords = get_node_coordinates(fmodel)
+
+function is_fine(n)
+  A = fcoords[n] ∉ ccoords
+  edges = n2e_map[n]
+  for e in edges
+    nbor_nodes = e2n_map[e]
+    A = A && all(m -> fcoords[m] ∉ ccoords,nbor_nodes)
+  end
+  return !A
+end
+
+_patches_mask = map(is_fine,LinearIndices(fcoords))
+patches_mask = reshape(_patches_mask,length(fcoords))
+Ih = PatchFESpace(Vh,PD,reffe;conformity=conformity,patches_mask=patches_mask)
+I_solver = PatchBasedLinearSolver(ap,Ih,Vh)
+I_ns = numerical_setup(symbolic_setup(I_solver,Ah),Ah)
+
+Ai = assemble_matrix(ap,Ih,Ih)
+
+
+function prolongate(dxH)
+  dxh = interp_c2f(dxH)
+  uh = FEFunction(Vh,dxh)
+
+  bh = assemble_vector(v -> graddiv(uh,v,dΩp),Ih)
+  dx̃ = Ai\bh
+
+  Pdxh = fill(0.0,length(dxh))
+  GridapSolvers.PatchBasedSmoothers.inject!(Pdxh,Ih,dx̃)
+  y = dxh - Pdxh
+
+  return y
+end
+
+# Solve
+
+xh = fill(1.0,size(Ah,2));
+rh = bh - Ah*xh
+niters = 20
+
+iter = 0
+error = norm(bh - Ah*xh)
+while iter < niters && error > 1.0e-8
+  println("Iter $iter:")
+  println(" > Initial: ", norm(rh))
+
+  solve!(xh,smoother_ns,rh)
+
+  println(" > Pre-smoother: ", norm(rh))
+
+  rH = project_f2c(rh)
+  qH = AH\rH
+  qh = prolongate(qH)
+
+  rh = rh - Ah*qh
+  xh = xh + qh
+  println(" > Post-correction: ", norm(rh))
+
+  solve!(xh,smoother_ns,rh)
+
+  iter += 1
+  error = norm(bh - Ah*xh)
+  println(" > Final: ",error)
+end
+
+uh = FEFunction(Uh,xh)
+eh = FEFunction(Vh,rh)
+uh_star = FEFunction(Uh,xh_star)
+#writevtk(Ωh,"data/h1div_error";cellfields=["eh"=>eh,"u"=>uh,"u_star"=>uh_star,"u_exact"=>u_exact])
+
+
+
+"""
+reffe_p = ReferenceFE(lagrangian,Float64,0;space=:P)
+QH = FESpace(cmodel,reffe_p;conformity=:L2)
+
+checks = fill(false,(num_free_dofs(QH),num_free_dofs(VH)))
+for i in 1:num_free_dofs(QH)
+  qH = zeros(num_free_dofs(QH)); qH[i] = 1.0
+  for j in 1:num_free_dofs(VH)
+    vH = zeros(num_free_dofs(VH)); vH[j] = 1.0
+    vh = interp_c2f(vH)
+    
+    ϕH = FEFunction(QH,qH)
+    φh = FEFunction(Vh,vh)
+    φH = FEFunction(VH,vH)
+    lhs = sum(∫(divergence(φh)*ϕH)*dΩh)
+    rhs = sum(∫(divergence(φH)*ϕH)*dΩh)
+    checks[i,j] = abs(lhs-rhs) < 1.0e-10
+  end
+end
+all(checks)
+
+reffe = LagrangianRefFE(VectorValue{2,Float64},QUAD,2)
+dof_ids = get_cell_dof_ids(Vh)
+local_dof_nodes = lazy_map(Reindex(reffe.reffe.dofs.nodes),reffe.reffe.dofs.dof_to_node)
+cell_maps = get_cell_map(fmodel)
+dof_nodes = Vector{VectorValue{2,Float64}}(undef,num_free_dofs(Vh))
+for (ids,cmap) in zip(dof_ids,cell_maps)
+  for (i,id) in enumerate(ids)
+    if id > 0
+      dof_nodes[id] = cmap(local_dof_nodes[i])
+    end
+  end
+end
+
+V̂h_dofs = findall(x -> !isempty(x),Ih.dof_to_pdof)
+checks = fill(false,(num_free_dofs(QH),length(V̂h_dofs)))
+for i in 1:num_free_dofs(QH)
+  qH = zeros(num_free_dofs(QH)); qH[i] = 1.0
+  for (j,j_dof) in enumerate(V̂h_dofs)
+    vh = zeros(num_free_dofs(Vh)); vh[j_dof] = 1.0
+    
+    ϕH = FEFunction(QH,qH)
+    φh = FEFunction(Vh,vh)
+    lhs = sum(∫(divergence(φh)*ϕH)*dΩHh)
+    checks[i,j] = abs(lhs) < 1.0e-10
+  end
+end
+all(checks)
+"""
