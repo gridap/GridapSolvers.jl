cc4f8aaea86b87fd9995acebef4973be7cb7302d
JordiManyer <jordi.manyer@monash.edu>
2024-03-25 23:19:39 +1100

    Added BlockSolvers docs

--- src/BlockSolvers/BlockDiagonalSolvers.jl
+++ src/BlockSolvers/BlockDiagonalSolvers.jl
@@ -1,4 +1,18 @@
+"""
+    struct BlockDiagonalSolver <: Gridap.Algebra.LinearSolver
 
+  Solver representing a block-diagonal solver, i.e 
+
+  [ A11   0   0  ] [ x1 ] = [ r1 ]
+  [  0   A22  0  ] [ x2 ] = [ r2 ]
+  [  0    0  A33 ] [ x3 ] = [ r3 ]
+
+  # Parameters: 
+  - `blocks::AbstractVector{<:SolverBlock}`: Matrix of solver blocks, indicating how 
+      each diagonal block of the preconditioner is obtained. 
+  - `solvers::AbstractVector{<:Gridap.Algebra.LinearSolver}`: Vector of solvers, 
+      one for each diagonal block.
+"""
 struct BlockDiagonalSolver{N,A,B} <: Gridap.Algebra.LinearSolver
   blocks  :: A
   solvers :: B
