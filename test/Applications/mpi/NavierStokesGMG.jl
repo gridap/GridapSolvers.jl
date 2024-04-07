26aa6f995e156034de102f40e4c69be510399bb3
JordiManyer <jordi.manyer@monash.edu>
2024-04-07 18:38:26 +1000

    Minor

--- test/Applications/mpi/NavierStokesGMG.jl
+++ test/Applications/mpi/NavierStokesGMG.jl
@@ -4,7 +4,7 @@ include("../NavierStokesGMG.jl")
 
 with_mpi() do distribute
   NavierStokesGMGApplication.main(distribute,4,(8,8))
-  #NavierStokesGMGApplication.main(distribute,4,(4,4,4))
+  NavierStokesGMGApplication.main(distribute,4,(4,4,4))
 end
 
 end
