043824c8177919639f34c18470a6d780e4b1448a
JordiManyer <jordi.manyer@monash.edu>
2024-07-04 18:00:31 +1000

    Moved to native refinement

--- test/Applications/mpi/DarcyGMG.jl
+++ test/Applications/mpi/DarcyGMG.jl
@@ -3,11 +3,11 @@ using MPI, PartitionedArrays
 include("../DarcyGMG.jl")
 
 with_mpi() do distribute
-  DarcyGMGApplication.main(distribute,4,(8,8),[4,1])
-  DarcyGMGApplication.main(distribute,4,(8,8),[4,2])
-  DarcyGMGApplication.main(distribute,4,(8,8),[4,4])
-  DarcyGMGApplication.main(distribute,4,(4,4,4),[4,1])
-  DarcyGMGApplication.main(distribute,4,(4,4,4),[4,4])
+  DarcyGMGApplication.main(distribute,4,(8,8),[(2,2),(1,1)])
+  DarcyGMGApplication.main(distribute,4,(8,8),[(2,2),(2,1)])
+  DarcyGMGApplication.main(distribute,4,(8,8),[(2,2),(2,2)])
+  DarcyGMGApplication.main(distribute,4,(4,4,4),[(2,2,1),(1,1,1)])
+  DarcyGMGApplication.main(distribute,4,(4,4,4),[(2,2,1),(2,2,1)])
 end
 
 end
--- test/Applications/mpi/StokesGMG.jl
+++ test/Applications/mpi/StokesGMG.jl
@@ -3,12 +3,12 @@ using MPI, PartitionedArrays
 include("../StokesGMG.jl")
 
 with_mpi() do distribute
-  StokesGMGApplication.main(distribute,4,(8,8),[4,1])
-  StokesGMGApplication.main(distribute,4,(8,8),[4,2])
-  StokesGMGApplication.main(distribute,4,(8,8),[4,4])
-  StokesGMGApplication.main(distribute,4,(4,4,4),[4,1])
-  StokesGMGApplication.main(distribute,4,(4,4,4),[4,2])
-  StokesGMGApplication.main(distribute,4,(4,4,4),[4,4])
+  StokesGMGApplication.main(distribute,4,(8,8),[(2,2),(1,1)])
+  StokesGMGApplication.main(distribute,4,(8,8),[(2,2),(2,1)])
+  StokesGMGApplication.main(distribute,4,(8,8),[(2,2),(2,2)])
+  StokesGMGApplication.main(distribute,4,(4,4,4),[(2,2,1),(1,1,1)])
+  StokesGMGApplication.main(distribute,4,(4,4,4),[(2,2,1),(2,1,1)])
+  StokesGMGApplication.main(distribute,4,(4,4,4),[(2,2,1),(2,2,1)])
 end
 
 end
