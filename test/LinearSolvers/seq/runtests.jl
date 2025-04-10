04e6b745625972244fea4ffb274766575f4a4ef3
JordiManyer <jordi.manyer@monash.edu>
2025-04-10 12:24:37 +1000

    Fixes

--- test/LinearSolvers/seq/runtests.jl
+++ test/LinearSolvers/seq/runtests.jl
@@ -1,8 +1,7 @@
 using Test
 
 include("KrylovTests.jl")
-include("IterativeSolversWrappersTests.jl")
+include("IterativeSolversExtTests.jl")
 include("SmoothersTests.jl")
 include("GMGTests.jl")
 include("RichardsonLinearTests.jl")
-
