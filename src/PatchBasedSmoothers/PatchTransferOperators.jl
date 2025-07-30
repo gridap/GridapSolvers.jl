0594817762fc0226dc42eb1b176152aee0935b2b
Jordi Manyer <jordi.manyer@monash.edu>
2025-07-31 09:33:16 +1000

    Minor

--- src/PatchBasedSmoothers/PatchTransferOperators.jl
+++ src/PatchBasedSmoothers/PatchTransferOperators.jl
@@ -13,13 +13,16 @@ function CoarsePatchTopology(
   ftopo = get_grid_topology(model)
   ctopo = get_grid_topology(Gridap.Adaptivity.get_parent(model))
   glue = Gridap.Adaptivity.get_adaptivity_glue(model)
+
+  # Each patch corresponds to a coarse cell. The fine patch cells are the 
+  # children of the coarse cell in the adaptivity glue.
   patch_cells = glue.o2n_faces_map[coarse_ids]
   
+  # The patch roots are given by the unique fine node that is interior to the coarse cell, 
+  # i.e the one that has it's cface dimension equal to Dc.
+  fcell_to_fnodes = Geometry.get_faces(ftopo,Dc,0)
   fnode_to_cface_dim = Gridap.Adaptivity.get_d_to_fface_to_cface(glue, ctopo, ftopo)[2][1]
   is_interior(n) = isequal(fnode_to_cface_dim[n],Dc)
-
-  Dc = num_cell_dims(model)
-  fcell_to_fnodes = Geometry.get_faces(ftopo,Dc,0)
   patch_roots = map(patch_cells) do cells
     nodes = filter(is_interior,intersect((fcell_to_fnodes[c] for c in cells)...))
     return only(nodes)
