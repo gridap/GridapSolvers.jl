bd143e00f35497d9f3eda91fab2aed1dae8b4b7e
Shreyas <shreyasprashanth2002@gmail.com>
2026-03-31 14:08:38 +0200

    update fix in richardsonlinearsolver

--- src/LinearSolvers/RichardsonLinearSolvers.jl
+++ src/LinearSolvers/RichardsonLinearSolvers.jl
@@ -11,7 +11,7 @@ The relaxation parameter (ω) can either be of type Float64 or Vector{Float64}.
 This gives flexiblity in relaxation.  
 """
 struct RichardsonLinearSolver<:Gridap.Algebra.LinearSolver
-  ω::Union{AbstractArray{Float64},Float64}
+  ω::Union{AbstractVector{Float64},Float64}
   Pl::Union{Gridap.Algebra.LinearSolver,Nothing}
   log::ConvergenceLog{Float64}
 end
